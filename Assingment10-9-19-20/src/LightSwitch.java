2d31f91d43a46319fc803f44e3f00c16d44f0743
Neda <nedataghiparast@gmail.com>
2020-10-13 17:50:27 -0700

    first commit

--- Assingment10-9-19-20/src/LightSwitch.java
+++ Assingment10-9-19-20/src/LightSwitch.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class LightSwitch {
+public class LightSwitch {
+    public int Id;
+    public boolean isOn;
+
+    public LightSwitch(int Id, boolean isOn) {
+        this.Id = Id;
+        this.isOn = isOn;
+
+    }
+
+    public int getId() {
+
+        return Id;
+    }
+
+    public void setId(int id) {
+
+        Id = id;
+    }
+
+    public boolean isOn() {
+
+        return isOn;
+    }
+
+    public void setIsOn() {
+        if (isOn == true) {
+            System.out.println("if isOn true should change to false");
+            this.isOn = false;
+        } else if (isOn == false) {
+            System.out.println("if isOn false should change to true");
+            this.isOn = true;
+        }
+    }
 }
+
--- Assingment10-9-19-20/src/Main.java
+++ Assingment10-9-19-20/src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        LightSwitch alexa = new LightSwitch(123987,false);
+        System.out.println(alexa.Id);
+        System.out.println(alexa.isOn);
+        alexa.setIsOn();
+        System.out.println(alexa.isOn());
+    }
 }
