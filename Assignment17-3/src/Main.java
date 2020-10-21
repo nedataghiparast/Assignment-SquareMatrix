bf1527be19d30ab8f81fdb14c172067a6464cadc
Neda <nedataghiparast@gmail.com>
2020-10-20 21:48:13 -0700

    first commit

--- Assignment17-3/src/Main.java
+++ Assignment17-3/src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+    char s[] = {'f','u','n'};
+        System.out.println(convertToString(s));
+    }
+
+
+    public static String convertToString(char[] a) {
+
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < a.length; i++){
+            str.append(a[i]);
+        }
+        return str.toString();
+    }
+
+
 }
