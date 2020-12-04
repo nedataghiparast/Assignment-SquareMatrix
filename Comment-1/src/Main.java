9c213f033330c33b08ef89c14d49230913e10d8b
Neda <nedataghiparast@gmail.com>
2020-12-03 23:16:07 -0800

    first commit

---
+++ Comment-1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+        /*
+JavaFX : - JavaFX is a Java library that consists of classes and interfaces that are written in
+java code. - JavaFX provides all the major UI controls that are required to develop a
+full-featured application. - JavaFX is intended to replace Swing as the standard GUI library
+ for java SE. - More Powerful. - Used to create both desktop and web applications. - Lightweight
+ and hardware-accelerated. - A media/graphics framework for creating GULs in java application. -
+ we use java because of much more improved advanced than swing. - JavaFX is more flexible and
+ more attractive and has much more functionality of Swing. - has Ability to apply CSS on the
+ components of the UI opens a door for better looking applications and also has 3D support.
+ JavaFXML : - we use JavaFXML to create any user interface. - JavaFXML is particularly useful
+ for user interfaces that have large complex scene graphs, forms, data entry. - FXML is an XML
+ format that JavaFX can use to import certain features of our GUI. - JavaFXML is not completed
+  language we do not need to recompile the code to see that changes.
+
+https://youtu.be/9YrmON6nlEw
+
+https://youtu.be/K7BOH-Ll8_g
+
+    */
+}
